2473057d9766be9875e1d3fa7e13b4bfaad5a0c3
Yash Pawar <yashpawarp@gmail.com>
2021-10-01 10:22:06 +0530

    refactor webrtc.py

--- webrtc.py
+++ webrtc.py
@@ -6,17 +6,20 @@ from rolling import roll
 from helper.compied import funcmain, draw_all
 from helper.face_detector import get_face_detector, find_faces, draw_faces
 
+
 face_model = get_face_detector()
 
 st.set_page_config(
-    page_title='Face Features and Landmarks Detection')
+    page_title = "Face Features and Landmarks Detection"
+)
 
-st.title('Facial Landmarks Detection App')
+st.title("Facial Landmarks Detection App")
 st.sidebar.title('Navigation')
-page = st.sidebar.selectbox("Select page:", options=[
-                            "Welcome", "Effects", "About"])
 
-
+page = st.sidebar.selectbox(
+    "Select page:", 
+    options = ["Welcome", "Effects", "About"]
+)
 
 
 if page == "Effects":
@@ -27,24 +30,16 @@ if page == "Effects":
     else:
         class VideoTransformer(VideoTransformerBase):
             effect_name = effect_name
-        
+
             def transform(self, frame):
                 img = frame.to_ndarray(format="bgr24")
-                if effect_name == "cartoonify":
-                    img = cartoonify(img)
-                elif effect_name == "negative":
-                    img = negative(img)
-                elif effect_name == "econify":
-                    img = econify(img)
-                elif effect_name == "watercolor":
-                    img = watercolor(img)
-                elif effect_name == "pencil":
-                    img = pencil(img)
-                    
-                elif effect_name == "canny":
-                    img = canny_img(img)
                 
-                
+                if effect_name == "cartoonify": img = cartoonify(img)
+                elif effect_name == "negative": img = negative(img)
+                elif effect_name == "econify": img = econify(img)
+                elif effect_name == "watercolor": img = watercolor(img)
+                elif effect_name == "pencil": img = pencil(img)
+                elif effect_name == "canny": img = canny_img(img)    
                 elif effect_name == "faces":
                     try:
                         rects = find_faces(img, face_model)
@@ -57,25 +52,29 @@ if page == "Effects":
                     except Exception as e:
                         print(e)
                 
-
-                
                 return img
 
 
-        ctxt = webrtc_streamer(client_settings = ClientSettings(
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-            media_stream_constraints={"video": True, "audio": False},),key="effects", video_transformer_factory=VideoTransformer)  
-
-
-
-
+        ctxt = webrtc_streamer(
+            client_settings = ClientSettings(
+                rtc_configuration = {
+                    "iceServers": [{
+                        "urls": ["stun:stun.l.google.com:19302"]
+                    }]
+                },
+                media_stream_constraints = {
+                    "video": True, 
+                    "audio": False
+                },
+            ),
+            key = "effects", 
+            video_transformer_factory = VideoTransformer
+        )  
 
 if page == 'Welcome':
     st.header("Welcome to the Neural Network based facial landmarks detection App!")
     welcome()
 
-
 if page == 'About':
     st.header("About section")
     about()
-
